01e2753959ac9cff7ce8aa2729d92078337e66bb
João Carlos <56844688+joaozeira@users.noreply.github.com>
2023-04-20 17:44:02 -0300

    Add files via upload

--- CLICKMASSA START/script/script.js
+++ CLICKMASSA START/script/script.js
@@ -24,35 +24,6 @@ window.addEventListener("scroll", function () {
   changeHeaderWhenScroll();
 });
 
-
-
-/* PLANOS */
-/* $(".content-planos").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: ".nav-planos",
-  swipe: false,
-  touchMove: false,
-  draggable: false,
-  infinite: false,
-});
-
-$(".nav-planos").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: ".content-planos",
-  dots: false,
-  arrows: false,
-  centerMode: false,
-  focusOnSelect: true,
-  swipeToSlide: false,
-  touchMove: false,
-  draggable: false,
-  infinite: false,
-}); */
-
 /* NAV RECURSOS */
 const gatilho = document.querySelector("#gatilho");
 const chamada = document.querySelector("#chamada");
@@ -90,47 +61,6 @@ interacao.addEventListener("click", function active() {
   contGatilho.style.display = 'none';
   contChamada.style.display = 'none';
   contInteracao.style.display = 'flex';
-});
-
-/* NAV PLANOS */
-const mensal = document.querySelector("#mensal");
-const semestral = document.querySelector("#semestral");
-const anual = document.querySelector("#anual");
-
-const cardMensal = document.querySelector("#cards-mensal");
-const cardSemestral = document.querySelector("#cards-semestral");
-const cardAnual = document.querySelector("#cards-anual");
-
-
-
-mensal.addEventListener("click", function active() {
-  mensal.classList.add("active");
-  semestral.classList.remove("active");
-  anual.classList.remove("active");
-
-  cardMensal.style.display = 'grid';
-  cardSemestral.style.display = 'none';
-  cardAnual.style.display = 'none';
-});
-
-semestral.addEventListener("click", function active() {
-  mensal.classList.remove("active");
-  semestral.classList.add("active");
-  anual.classList.remove("active");
-
-  cardMensal.style.display = 'none';
-  cardSemestral.style.display = 'grid';
-  cardAnual.style.display = 'none';
-});
-
-anual.addEventListener("click", function active() {
-  mensal.classList.remove("active");
-  semestral.classList.remove("active");
-  anual.classList.add("active");
-
-  cardMensal.style.display = 'none';
-  cardSemestral.style.display = 'none';
-  cardAnual.style.display = 'grid';
 });
 
 
